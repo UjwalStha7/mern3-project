e74c117664d8becce9b534a7a4b782745fd14374
Ujwal Shrestha <sthaujwal07@gmail.com>
2026-05-06 21:12:02 +0545

    image upload api

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ app.use(express.json());
 
 connectDB();    
 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(200).json({
+        message : "Hello World!"
+    });
 });
 
 app.post("/blog", async (req, res) => {
@@ -31,12 +37,43 @@ app.post("/blog", async (req, res) => {
     res.send("Blog api hit successfully");
 });
 
-app.post("/upload", upload.single('image'), (req, res) => {
+// app.post("/upload", upload.single('image'), (req, res) => {
+//     res.status(200).json({
+//         message : "File uploaded successfully",
+//     });
+// });
+
+app.post("/upload", upload.single('image'), async (req, res) => {
+    const { title, subtitle, description} = req.body;
+    const filename = req.file.filename;
+    // const {filename} = req.body;   destructuring
+    if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message : "Please provide all the required fields"
+        });
+    }
+    await Blog.create({
+        title : title,  //if name of key and value is same we can just write title
+        subtitle : subtitle,
+        description : description,
+        image : filename
+    });
+    console.log(req.body); 
+    console.log(req.file);
     res.status(200).json({
         message : "File uploaded successfully",
-        file : req.file
-    });
+    }); b
 });
+
+app.get("/upload", async (req, res) => {
+    const blogs = await Blog.find(); //returns array
+    res.status(200).json({
+        message : "Blogs fetched successfully",
+        data : blogs
+    });
+})
+
+app.use(express.static('./storage'));
 
 app.listen(port, () => {  
     console.log(`App listening at http://localhost:${port}`);
